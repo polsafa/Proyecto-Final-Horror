b9eddf5504aaaadab573d18284428f84e2e30ce6
polsafa <nork_pol@hotmail.es>
2020-02-04 18:48:35 +0100

    add objectives

---
+++ Final Project/Assets/Scripts/Logros.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+public class Logros : MonoBehaviour
+{
+public class Archivements
+    {
+        public string scenename;
+        public int status;
+
+        public List<Archivements>  ReadArchivements()
+        {
+            List<Archivements> archiveInfo = new List<Archivements>();
+            
+     
+            return archiveInfo;
+        }
+        public void read()
+        {
+            StreamReader reader = new StreamReader("./objectives.txt");
+            string itemStrings = reader.ReadLine();
+            char[] delimiter = { ';' };
+            string[] fields = itemStrings.Split(delimiter);
+            while (itemStrings != null)
+            {
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    Debug.Log(fields[i]);
+                }
+                itemStrings = reader.ReadLine();
+            }
+           
+        }
+
+
+    }
+}
